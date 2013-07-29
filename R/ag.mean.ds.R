45cd3b14929d8de4b03f9f9d8372f43436235020
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-07-29 15:14:17 +0100

    First commit in my dev space

---
+++ R/ag.mean.ds.R
@@ -0,0 +1,16 @@
+#' Computes the mean statistic for vectors with more than 4 entries
+#'
+#' @title Calculates a statistical mean
+#' @param \code{a} a numerical vector
+#' @return a numeric 
+#' @author A. Gaye 
+#' @export
+#' 
+
+ag.mean.ds <- function (a) {
+  if(length(a) >= 5){
+    mean(a,na.rm=TRUE)
+  }else{
+    cat("Operation not allowed!\n")
+  }
+}
