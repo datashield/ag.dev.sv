af485d36feb79d540b3d2a8605f0cb07f369f5e9
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-08-17 03:09:48 +0100

    Function 'MinMax.ds' added

---
+++ R/ag.MinMax.ds.R
@@ -0,0 +1,27 @@
+#' 
+#' @title Generates a matrix with a minimum and maximum value, for two numerical vector
+#' @description This function used by another function to plot a heatmap graph.
+#' @param xvect a numerical vector
+#' @param yvect a numerical vector
+#' @return a matrix
+#' @author Isaeva, J.
+#' @export
+#' 
+ag.MinMax.ds <- function(xvect, yvect) {
+  
+  MinMaxMatrix <- data.frame(cbind(c(0,0),c(0,0)))
+  colnames(MinMaxMatrix) <- c(as.character(quote(xvect)), as.character(quote(yvect)))
+  rownames(MinMaxMatrix) <- c("min", "max")
+  
+  MinMaxMatrix[1, 1] <- min(xvect, na.rm=T)
+  MinMaxMatrix[2, 1] <- max(xvect, na.rm=T)
+  MinMaxMatrix[1, 2] <- min(yvect, na.rm=T)
+  MinMaxMatrix[2, 2] <- max(yvect, na.rm=T)
+  
+  MinMaxMatrix[1, 1] <- runif(1, 0.9, 1) * MinMaxMatrix[1, 1]
+  MinMaxMatrix[2, 1] <- runif(1, 1, 1.1) * MinMaxMatrix[2, 1] 
+  MinMaxMatrix[1, 2] <- runif(1, 0.9, 1) * MinMaxMatrix[1, 2]
+  MinMaxMatrix[2, 2] <- runif(1, 1, 1.1) * MinMaxMatrix[2, 2]
+  
+  return(MinMaxMatrix)  
+}
