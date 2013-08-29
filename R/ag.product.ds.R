42ab7bc92039441163a3770b77ef45f8f5a1ebee
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-08-29 23:43:55 +0100

    Bug in description file corrected and function 'product.ds' added

---
+++ R/ag.product.ds.R
@@ -0,0 +1,14 @@
+#' 
+#' @title Computes a product
+#' @param a a numerical 
+#' @param b a numerical 
+#' @param c a numerical 
+#' @param d a numerical 
+#' @param e a numerical
+#' @return a numerical
+#' @author Burton, P.
+#' @export
+#' 
+ag.product.ds <- function (a=1,b=1,c=1,d=1,e=1){
+  a*b*c*d*e
+}
