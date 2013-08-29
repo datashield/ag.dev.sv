33afee2f1d9fdd2453bf7b0d70930af52327eb45
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-08-29 19:59:17 +0100

    Code of the function 't.test.ds' replace and R function 't.test' made available to client.

--- R/ag.t.test.ds.R
+++ R/ag.t.test.ds.R
@@ -1,7 +1,7 @@
 #' 
-#' @title Performs one and two sample t-tests
-#' @param xvect a numeric vector of data values
-#' @param yvect a numeric vector of data values
+#' @title Produces the input required by the function 'ds.t.test'
+#' @param x a numeric vector of data values
+#' @param y a numeric vector of data values
 #' @param alternative an integer specifying the alternative hypothesis,
 #' 1 for "two.sided" (default), 2 for "greater" or 3 for "less".  
 #' @param mu a number indicating the true value of the mean (or difference
@@ -12,9 +12,8 @@
 #' is used to estimate the variance otherwise the Welch (or satterthwaite) 
 #' approximation to the degrees of freedom is used.
 #' @param conf.level confidence level of the interval.
-#' @return a list of class \code{htest}, see the R function \code{t.test} in 
-#' package \code{stats} for details of the elements in the returmed object.
-#' @author Burton P.; Gaye, A.
+#' @return a list that contains the length, mean and variance of the variable x
+#' @author Gaye, A., Isaeva, J.
 #' @export
 #' @examples 
 #' \dontrun{
@@ -29,11 +28,45 @@
 #' datashield.aggregate(opals, quote(t.test.ds(D$LAB_TSC, D$LAB_HDL)))
 #' }
 #'
-ag.t.test.ds <- function (xvect, yvect = NULL, alternative = 1, mu = 0, paired = FALSE, var.equal = FALSE,
-                    conf.level = 0.95) {
-  if(alternative == 1){alternative <- "two.sided"}
-  if(alternative == 2){alternative <- "less"}
-  if(alternative == 3){alternative <- "greater"}
-  output <- t.test(xvect, yvect, alternative, mu, paired, var.equal, conf.level)
-  return (output)
+ag.t.test.ds <- function (x, y = NULL, alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95){  
+  
+  if (!missing(mu) && (length(mu) != 1 || is.na(mu))){
+    stop("'mu' must be a single number")
+  }
+  
+  if (!missing(conf.level) && (length(conf.level) != 1 || !is.finite(conf.level) || conf.level < 0 || conf.level > 1)){
+    stop("'conf.level' must be a single number between 0 and 1")
+  }
+  
+  if (!is.null(y)){
+    if (paired){
+      pair.compl.obs <- complete.cases(x, y)
+      xok <- subset(x, pair.compl.obs)
+      yok <- subset(x, pair.compl.obs)
+    }else{
+      not.na.x <- complete.cases(x)
+      xok <- subset(x, not.na.x)
+      not.na.y <- complete.cases(y)    
+      yok <- subset(x, not.na.y)
+    }
+  }else{
+    if (paired){
+      stop(paste(y, " is missing for paired test", sep=""))
+    }
+    not.na.x <- complete.cases(x)
+    xok <- subset(x, not.na.x)    
+  }
+  
+  if (paired){
+    minus_y <- yok * (-1)
+    xok <- sum(xok, minus_y)
+    yok <- as.null(yok)
+  }
+  
+  length.local.x <- NROW(xok)
+  mean.local.x <- mean(xok, na.rm=TRUE)
+  var.local.x <- var(xok, na.rm=TRUE)  
+  
+  return(list("length.local.x"=length.local.x, "mean.local.x"=mean.local.x, "var.local.x"=var.local.x))
+  
 }
